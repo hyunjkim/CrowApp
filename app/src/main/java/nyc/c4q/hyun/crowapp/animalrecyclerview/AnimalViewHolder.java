ea6901be30a2be0834e84598d263e561c9cf6cf5
hyunjkim <hkim6501@gmail.com>
2016-12-21 21:42:48 -0500

    background linearlayout changes only

--- app/src/main/java/nyc/c4q/hyun/crowapp/animalrecyclerview/AnimalViewHolder.java
+++ app/src/main/java/nyc/c4q/hyun/crowapp/animalrecyclerview/AnimalViewHolder.java
@@ -1,6 +1,7 @@
 package nyc.c4q.hyun.crowapp.animalrecyclerview;
 
 import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -14,14 +15,26 @@ import nyc.c4q.hyun.crowapp.animals.Animals;
 public class AnimalViewHolder extends RecyclerView.ViewHolder {
 
     private TextView animalName;
+    private CardView animalBackgroundColor;
 
     public AnimalViewHolder(View view) {
         super(view);
         animalName = (TextView) view.findViewById(R.id.name_TV);
+        animalBackgroundColor = (CardView) view.findViewById(R.id.card_view);
     }
 
-    public void showAnimals(Animals animals) {
+    public void showAnimals(final Animals animals) {
         animalName.setText(animals.getName());
         animalName.setTextColor(Color.parseColor(animals.getTextColor()));
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                animalBackgroundColor.setBackgroundColor(Color.parseColor(animals.getBackground()));
+                nameClicked(animals,view);
+            }
+        });
+    }
+    public void nameClicked(Animals animals, View view) {
+        view.setBackgroundColor(Color.parseColor(animals.getBackground()));
     }
 }
